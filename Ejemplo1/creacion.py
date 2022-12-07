5390b9ffb8824c560da9bfd8093809093cc4976b
SebasVGC <2902340180301@ingenieria.usac.edu.gt>
2022-12-07 16:01:35 -0600

    ejemplo v2

--- Ejemplo1/creacion.py
+++ Ejemplo1/creacion.py
@@ -1,7 +1,52 @@
-DROP_TABLES=('DROP TABLE if exists temporal;')
+DROP_TABLES=('DROP TABLE if exists temporal;DROP TABLE if exists plays;DROP TABLE if exists song;DROP TABLE if exists artist;DROP TABLE if exists genre;')
 
 TEMPORAL_CREATION=('CREATE TABLE temporal(artist VARCHAR(50),song VARCHAR(500),duration_ms int,explicit bit,'
     'year int,popularity decimal(10,4),danceability decimal(10,4),energy decimal(10,4),llave int,' 
     'loudness decimal(10,4),mode decimal(10,4),speechiness decimal(10,4),acousticness decimal(10,4),'
     'instrumentalness decimal(10,7),liveness decimal(10,4),valence decimal(10,4),tempo decimal(10,4),'
     'genre varchar(50) )')
+
+ARTIST_CREATION=('CREATE TABLE artist ('
+    'id INTEGER IDENTITY(1,1) PRIMARY KEY,'
+    'name_ VARCHAR(50)'
+    ');')
+
+
+GENRE_CREATION=('CREATE TABLE genre ('
+    'id INTEGER IDENTITY(1,1) PRIMARY KEY,'
+    'name_ VARCHAR(50)'
+    ');')
+
+SONG_CREATION=('CREATE TABLE song ('
+    'id INTEGER IDENTITY(1,1) PRIMARY KEY,'
+    'name_ VARCHAR(500),'
+    'duration_ms int,explicit bit,'
+    'year int,popularity decimal(10,4),danceability decimal(10,4),energy decimal(10,4),llave int,' 
+    'loudness decimal(10,4),mode decimal(10,4),speechiness decimal(10,4),acousticness decimal(10,4),'
+    'instrumentalness decimal(10,7),liveness decimal(10,4),valence decimal(10,4),tempo decimal(10,4),'
+    'artist_id INTEGER,'
+    'genre_id INTEGER,'
+    ');')
+
+PLAYS_CREATION=('CREATE TABLE plays ('
+    'id INTEGER IDENTITY(1,1) PRIMARY KEY,'
+    'song_id INTEGER'
+    ');')
+
+FK_CREATION1=('ALTER TABLE song '
+    'ADD CONSTRAINT song_artist_fk FOREIGN KEY ( artist_id )'
+    'REFERENCES artist ( id )'
+    'ON DELETE CASCADE;'
+    ';')
+
+FK_CREATION2=('ALTER TABLE song '
+    'ADD CONSTRAINT song_genre_fk FOREIGN KEY ( genre_id )'
+    'REFERENCES genre ( id )'
+    'ON DELETE CASCADE;'
+    ';')
+
+FK_CREATION3=('ALTER TABLE plays '
+    'ADD CONSTRAINT plays_song_fk FOREIGN KEY ( song_id )'
+    'REFERENCES song ( id )'
+    'ON DELETE CASCADE;'
+    ';')
--- Ejemplo1/main.py
+++ Ejemplo1/main.py
@@ -12,7 +12,7 @@ import logging
 
 logger = logging.getLogger('Semi_2_Ejemplo')
 logger.setLevel(logging.DEBUG)
-ch = logging.FileHandler('logs.log')
+ch = logging.FileHandler('logs.log', 'w',encoding = 'UTF-8')
 ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
@@ -33,6 +33,8 @@ conn = pyodbc.connect(CONNECTION_STRING,autocommit=True)
 logger.info("Conexion realizada con exito")
 
 def main():
+    archivo=open('resultados.txt','w',encoding="utf-8")
+    archivo.close()
     menu()
 
 def menu():
@@ -40,11 +42,15 @@ def menu():
         print_main_menu()
         opcion = input('Seleccione una opcion: ')
         if opcion=='1':
-            logger.info("Usuario selecciona opcion de creacion")
+            logger.info("Usuario selecciona opcion de creacion de modelo")
             creacion()
         elif opcion=='2':
             logger.info('Usuario selecciona opcion de ejecutar consultas')
-            consultas(CONSULTA1)
+            creacion2()
+        elif opcion=='3':
+            logger.info('Usuario selecciona opcion de ejecutar consultas')
+            consultas(CONSULTA1,"SELECT * FROM")
+
         else:
             conn.close()
             logger.info('Conexion finalizada')
@@ -61,7 +67,7 @@ def creacion():
     cursor.execute(TEMPORAL_CREATION)
     logger.info("Comenzando a procesar el dataset")
     try:
-        data = pd.read_csv("songs_normalize.csv")
+        data = pd.read_csv("songs_normalize.csv", encoding='utf-8')
         df = pd.DataFrame(data)
         logger.info("Dataset leido exitosamente")
         cargar_temporal(df)
@@ -74,9 +80,10 @@ def cargar_temporal(df):
     cursor = conn.cursor()
     i = 0
     for row in df.itertuples():
-        if(i==0):
-            i = i +1
-            continue
+        i = i +1
+        # if(i==0):
+        #     i = i +1
+        #     continue
         artist = str(row[1].replace("'","''"))
         song = str(row[2].replace("'","''"))
         genre = str(row[18].replace("'","''"))
@@ -88,19 +95,104 @@ def cargar_temporal(df):
     #logger(cursor.execute("SELECT * FROM temporal LIMIT 1"))
     logger.info(f'Se insertaron correctamente {i} filas')
 
-def consultas(consulta_):
+def creacion2():
+    logger.info("Procediendo a crear modelo...")
+    cursor = conn.cursor()
+    logger.info("Creando las tablas necesarias")
+    cursor.execute(ARTIST_CREATION)
+    cursor.execute(GENRE_CREATION)
+    cursor.execute(SONG_CREATION)
+    cursor.execute(PLAYS_CREATION)
+    cursor.execute(FK_CREATION1)
+    cursor.execute(FK_CREATION2)
+    cursor.execute(FK_CREATION3)
+    logger.info("Comenzando a procesar el dataset")
+    cargar_artistas()
+    cargar_generos()
+    cargar_canciones()
+    cargar_plays()
+
+def cargar_artistas():
+    cursor = conn.cursor()
+    query = (f'INSERT INTO artist SELECT DISTINCT artist FROM temporal')
+    logger.info(query)
+    try:
+        cursor.execute(query)
+        logger.info(f'Se insertaron correctamente los artistas')
+    except Exception as e:
+        logger.error(e)
+        conn.close()
+        exit()
+
+def cargar_generos():
+    cursor = conn.cursor()
+    query = (f'INSERT INTO genre SELECT DISTINCT genre FROM temporal')
+    logger.info(query)
+    try:
+        cursor.execute(query)
+        logger.info(f'Se insertaron correctamente los generos')
+    except Exception as e:
+        logger.error(e)
+        conn.close()
+        exit()
+
+def cargar_canciones():
+    cursor = conn.cursor()
+    query = (f'INSERT INTO song SELECT DISTINCT t.song,t.duration_ms,t.explicit,t.year,t.popularity,t.danceability,t.energy,t.llave,t.loudness,t.mode,t.speechiness,t.acousticness,t.instrumentalness,t.liveness,t.valence,t.tempo,a.id,g.id FROM temporal t, artist a, genre g WHERE t.artist=a.name_ AND t.genre=g.name_ ;')
+    logger.info(query)
+    try:
+        cursor.execute(query)
+        logger.info(f'Se insertaron correctamente las canciones')
+    except Exception as e:
+        logger.error(e)
+        conn.close()
+        exit()
+
+def cargar_plays():
+    cursor = conn.cursor()
+    query = (f'SELECT * FROM temporal')
+    try:
+        cursor.execute(query)
+        resultado=cursor.fetchall()
+        for row_ in resultado:
+            # print(item.song)
+            song = str(row_.song.replace("'","''"))
+            query = (f'INSERT INTO plays SELECT s.id FROM song s WHERE ( s.name_=\'{song}\' AND s.year={row_.year} AND s.popularity={row_.popularity} AND s.tempo={row_.tempo} )')
+            logger.info(query)
+            cursor.execute(query)
+
+        logger.info(f'Se insertaron correctamente las reproducciones')
+    except Exception as e:
+        logger.error(e)
+        conn.close()
+        exit()
+
+
+def consultas(consulta_,titulo_):
     try:
         cursor = conn.cursor()
         query = consulta_
         logger.info(query)
         cursor.execute(query)
         data = cursor.fetchall()
-        print(data)
-        
+        # print(data)
+        archivo=open('resultados.txt','a',encoding="utf-8")
+        cadena=""
+        cadena+=titulo_+"\n"
+        for row_ in data:
+            cadena+="\t"
+            for element_ in row_:
+                cadena+=str(element_)+"\t"
+            cadena+="\n"
+        cadena+="\n"
+        archivo.write(cadena)
+        archivo.close()
+
     except Exception as e:
         logger.error(e)
         conn.close()
         exit()
+
 
 if __name__ == "__main__":
     main()
